18a2c121154d588819485155ea0e5dabc7df804c
nbnsireesha <nbnsireesha@gmail.com>
2018-04-14 16:24:59 -0500

    done with side nav bar

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -4,3 +4,21 @@ function openNavStory() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+function openNavHelp(){
+	document.getElementById("mySidenavHelp").style.width = "250px";
+}
+function closeNavHelp(){
+	document.getElementById("mySidenavHelp").style.width = "0";
+}
+function openNavGoal(){
+	document.getElementById("mySidenavGoal").style.width = "250px";
+}
+function closeNavGoal(){
+	document.getElementById("mySidenavGoal").style.width = "0";
+}
+function openNavJoin(){
+	document.getElementById("mySidenavJoin").style.width = "250px";
+}
+function closeNavJoin(){
+	document.getElementById("mySidenavJoin").style.width = "0";
+}
